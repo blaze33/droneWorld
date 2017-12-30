bd57016ebcd12ee484eb5d0b8ddd0ce5ddca0d0b
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-12-30 01:43:59 +0100

    Added THREE global for shader-particle-engine

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -333,6 +333,9 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.ProvidePlugin({
+      THREE: "three",
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
