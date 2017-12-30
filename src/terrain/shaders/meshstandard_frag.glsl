1adc42900e5c1514be9b5cb773d898f9134534b5
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-12-30 22:30:54 +0100

    fixed grass normal map used on rock

--- src/terrain/shaders/meshstandard_frag.glsl
+++ src/terrain/shaders/meshstandard_frag.glsl
@@ -45,7 +45,38 @@ varying vec3 vNormal2;
 #include <lights_physical_pars_fragment>
 #include <shadowmap_pars_fragment>
 #include <bumpmap_pars_fragment>
-#include <normalmap_pars_fragment>
+
+// #include <normalmap_pars_fragment>
+#ifdef USE_NORMALMAP
+
+	uniform sampler2D normalMap;
+	uniform vec2 normalScale;
+
+	// Per-Pixel Tangent Space Normal Mapping
+	// http://hacksoflife.blogspot.ch/2009/11/per-pixel-tangent-space-normal-mapping.html
+
+	vec3 perturbNormal2Arb( vec3 eye_pos, vec3 surf_norm , sampler2D normalMap) {
+
+		// Workaround for Adreno 3XX dFd*( vec3 ) bug. See #9988
+
+		vec3 q0 = vec3( dFdx( eye_pos.x ), dFdx( eye_pos.y ), dFdx( eye_pos.z ) );
+		vec3 q1 = vec3( dFdy( eye_pos.x ), dFdy( eye_pos.y ), dFdy( eye_pos.z ) );
+		vec2 st0 = dFdx( vUv.st );
+		vec2 st1 = dFdy( vUv.st );
+
+		vec3 S = normalize( q0 * st1.t - q1 * st0.t );
+		vec3 T = normalize( -q0 * st1.s + q1 * st0.s );
+		vec3 N = normalize( surf_norm );
+
+		vec3 mapN = texture2D( normalMap, vUv ).xyz * 2.0 - 1.0;
+		mapN.xy = normalScale * mapN.xy;
+		mat3 tsn = mat3( S, T, N );
+		return normalize( tsn * mapN );
+
+	}
+
+#endif
+
 #include <roughnessmap_pars_fragment>
 #include <metalnessmap_pars_fragment>
 #include <logdepthbuf_pars_fragment>
@@ -63,7 +94,37 @@ vec4 physicalColor(sampler2D map, sampler2D normalMap, float roughness, float me
 	#include <alphatest_fragment>
 	#include <roughnessmap_fragment>
 	#include <metalnessmap_fragment>
-	#include <normal_fragment>
+	// #include <normal_fragment>
+	#ifdef FLAT_SHADED
+
+		// Workaround for Adreno/Nexus5 not able able to do dFdx( vViewPosition ) ...
+
+		vec3 fdx = vec3( dFdx( vViewPosition.x ), dFdx( vViewPosition.y ), dFdx( vViewPosition.z ) );
+		vec3 fdy = vec3( dFdy( vViewPosition.x ), dFdy( vViewPosition.y ), dFdy( vViewPosition.z ) );
+		vec3 normal = normalize( cross( fdx, fdy ) );
+
+	#else
+
+		vec3 normal = normalize( vNormal );
+
+		#ifdef DOUBLE_SIDED
+
+			normal = normal * ( float( gl_FrontFacing ) * 2.0 - 1.0 );
+
+		#endif
+
+	#endif
+
+	#ifdef USE_NORMALMAP
+
+		normal = perturbNormal2Arb( -vViewPosition, normal , normalMap);
+
+	#elif defined( USE_BUMPMAP )
+
+		normal = perturbNormalArb( -vViewPosition, normal, dHdxy_fwd() );
+
+	#endif
+
 	#include <emissivemap_fragment>
 
 	// accumulation
@@ -87,7 +148,7 @@ void main() {
 	vec4 grassColor = physicalColor(map, normalMap, roughness, metalness);
 	vec4 rockColor = physicalColor(rockTexture, rockTextureNormal, 0.4, 0.15);
 	vec3 normal = normalize( vNormal2 );
-	normal = perturbNormal2Arb( -vViewPosition, normal );
+	// normal = perturbNormal2Arb( -vViewPosition, normal );
 	float flatness = dot(normal, vec3(0.0, 0.0, 1.0));
 	vec4 colorTerrain = mix(
 		rockColor,
