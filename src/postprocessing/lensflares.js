6a407c1f5478fecf5cea0ed42f38f3d324ee0425
Maxime Rouyrre <rouyrre+git@gmail.com>
2018-05-12 12:36:43 +0200

    Simplified lensflares

--- src/postprocessing/lensflares.js
+++ src/postprocessing/lensflares.js
@@ -1,7 +1,5 @@
 import {
-  TextureLoader,
-  Color,
-  AdditiveBlending
+  TextureLoader
 } from 'three'
 import {
     Lensflare,
@@ -9,19 +7,9 @@ import {
 } from '../modules/Lensflare'
 
 const textureLoader = new TextureLoader()
-
-const textureFlare0 = textureLoader.load(require('../textures/lensflare/lensflare0.png'))
-const textureFlare2 = textureLoader.load(require('../textures/lensflare/lensflare2.png'))
 const textureFlare3 = textureLoader.load(require('../textures/lensflare/lensflare3.png'))
 
-const flareColor = new Color(0xffffff)
-// flareColor.setHSL( h, s, l + 0.5 );
-
-const lensFlare = new Lensflare(textureFlare0, 350, 0.0, AdditiveBlending, flareColor)
-console.log(lensFlare)
-lensFlare.addElement(new LensflareElement(textureFlare2, 512, 0.0))
-lensFlare.addElement(new LensflareElement(textureFlare2, 512, 0.0))
-lensFlare.addElement(new LensflareElement(textureFlare2, 512, 0.0))
+const lensFlare = new Lensflare()
 
 lensFlare.addElement(new LensflareElement(textureFlare3, 60, 0.6))
 lensFlare.addElement(new LensflareElement(textureFlare3, 70, 0.7))
