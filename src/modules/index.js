a25d014c90e3d9d9357c8924d92f09056ed08b77
Maxime Rouyrre <rouyrre+git@gmail.com>
2018-05-28 23:09:28 +0200

    added Octree module

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,11 +4,14 @@ global.THREE = THREE
 
 require('three/examples/js/loaders/GLTFLoader')
 require('three/examples/js/objects/Sky.js')
+require('three/examples/js/Octree.js')
 
 const GLTFLoader = global.THREE.GLTFLoader
 const Sky = global.THREE.Sky
+const Octree = global.THREE.Octree
 
 export {
   GLTFLoader,
-  Sky
+  Sky,
+  Octree
 }
