5daa1c3b45169c9c7d36599fd99e1e24124e295a
Maxime Rouyrre <rouyrre+git@gmail.com>
2018-06-24 00:31:33 +0200

    Added clouds shader

--- src/postprocessing/index.js
+++ src/postprocessing/index.js
@@ -2,10 +2,12 @@ import initDoF from './dof'
 import lensFlare from './lensflares'
 
 import {motionBlurShader} from './MotionBlur'
+import CloudsShader from './Clouds'
 
 export {
   initDoF,
   lensFlare,
 
-  motionBlurShader
+  motionBlurShader,
+  CloudsShader
 }
