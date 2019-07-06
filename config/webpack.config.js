e86e28c6f501dbc6cb2cf1d0a9268bc0b9109c66
Maxime Rouyrre <rouyrre+git@gmail.com>
2019-07-06 22:26:10 +0200

    Revert "switched to preact"
    This reverts commit 9de5388b7da49c75a179925970ac53ea2026419c.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -276,9 +276,7 @@ module.exports = function (webpackEnv) {
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-        'react-native': 'react-native-web',
-        'react': 'preact-compat',
-        'react-dom': 'preact-compat'
+        'react-native': 'react-native-web'
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
