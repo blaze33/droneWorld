ab56c40256d57634bd56fa6c119fc3d2edec5285
Maxime Rouyrre <rouyrre+git@gmail.com>
2019-07-06 18:13:19 +0200

    shimming THREE global variable for SPE

---
+++ src/particles/three.js
@@ -0,0 +1,16 @@
+// THREE.js dependecies from SPE
+export {
+  BufferAttribute,
+  REVISION,
+  ShaderChunk,
+  Vector2,
+  Vector3,
+  Vector4,
+  Color,
+  Texture,
+  Math,
+  AdditiveBlending,
+  ShaderMaterial,
+  BufferGeometry,
+  Points
+} from 'three'
