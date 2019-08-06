dcd6c962fd0252ff745a3726d22a7feb95491f65
Maxime Rouyrre <rouyrre+git@gmail.com>
2019-08-07 01:13:56 +0200

    terrain worker: offload work to wasm/rust

--- src/terrain/terrain.worker.js
+++ src/terrain/terrain.worker.js
@@ -1,9 +1,7 @@
 /* eslint-env worker */
 
-import { PlaneBufferGeometry } from 'three/src/geometries/PlaneGeometry'
-import UPNG from 'upng-js'
-// import { SimplifyModifier } from 'three/examples/jsm/modifiers/SimplifyModifier.js'
-import { crackFix } from './crackFix'
+import { BufferGeometry } from 'three/src/core/BufferGeometry'
+import { BufferAttribute } from 'three/src/core/BufferAttribute'
 
 const messages = []
 onmessage = message => messages.push(message)
@@ -20,15 +18,7 @@ import('./dem2mesh/pkg').then(pkg => {
 })
 
 const tilesElevationURL = 'https://s3.amazonaws.com/elevation-tiles-prod/terrarium'
-// const simple = new SimplifyModifier()
 
-const pngToHeight = (array) => {
-  const heightmap = new Float32Array(256 * 256)
-  for (let i = 0, j = array.length; i < j; i += 4) {
-    heightmap[i / 4] = array[i] * 256.0 + array[i + 1] + array[i + 2] / 256.0 - 32768.0
-  }
-  return heightmap
-}
 const offsetCoords = (z, x, y) => {
   const maxTile = Math.pow(2, z)
   const offset = offsetAtZ(z)
@@ -41,55 +31,29 @@ const offsetCoords = (z, x, y) => {
   }
   return [z, x, y]
 }
-const heightmap = (z, x, y) => {
+const fetchPNG = (z, x, y) => {
   [z, x, y] = offsetCoords(z, x, y)
   const tileURL = `${tilesElevationURL}/${z}/${x}/${y}.png`
   return fetch(tileURL)
     .then(res => res.arrayBuffer())
-    .then(array => {
-      return dem2mesh.png2elevation(new Uint8Array(array))
-      // const t0 = Date.now()
-      // const retwasm = dem2mesh.png2elevation(new Uint8Array(array))
-      // const t1 = Date.now()
-      // const retjs = pngToHeight(new Uint8Array(UPNG.toRGBA8(UPNG.decode(array))[0]))
-      // const t2 = Date.now()
-      // postMessage({stats: true, wasm: t1-t0, js: t2-t1})
-      // return retwasm
-    })
+    .then(arrayBuffer => new Uint8Array(arrayBuffer))
 }
 
-const setHeightmap = (geometry, heightmap, scale, offset, key) => {
-  if (!geometry) { return }
-  const nPosition = Math.sqrt(geometry.attributes.position.count)
-  const nHeightmap = Math.sqrt(heightmap.length)
-  const ratio = nHeightmap / (nPosition)
-  let x, y
-  for (let i = nPosition; i < geometry.attributes.position.count - nPosition; i++) {
-    if (
-      i % (nPosition) === 0 ||
-      i % (nPosition) === nPosition - 1
-    ) continue
-    x = Math.floor(i / (nPosition))
-    y = i % (nPosition)
-    geometry.attributes.position.setZ(
-      i,
-      heightmap[Math.round(Math.round(x * ratio) * nHeightmap + y * ratio)] * scale + offset
-    )
-  }
+const buildTile = (png, size, segments, key) => {
+  let geometry = new BufferGeometry()
+  let position
+  let index
 
-  // center geometry along xY for correct XY scaling in crackFix
-  const z0 = geometry.attributes.position.array[2]
-  geometry.center()
-  const z1 = geometry.attributes.position.array[2]
-  const deltaZ = z0 - z1
-  geometry.translate(0, 0, deltaZ)
-  geometry.scale(1, 1, 0.75)
-  crackFix(geometry)
+  [position, index] = dem2mesh.png2mesh(png, size, segments)
 
-  // dem2mesh.greet()
-  // const target = Math.floor(geometry.attributes.position.count * 0.4)
-  // geometry = simple.modify(geometry, target)
-  // geometry.computeVertexNormals()
+  geometry.addAttribute(
+    'position',
+    new BufferAttribute(Float32Array.from(position), 3)
+  )
+  geometry.setIndex(
+    new BufferAttribute(Uint16Array.from(index), 1)
+  )
+  geometry.computeVertexNormals()
 
   const positions = geometry.attributes.position.array.buffer
   const normals = geometry.attributes.normal.array.buffer
@@ -126,10 +90,7 @@ const offsetAtZ = (z) => {
 }
 
 const buildPlane = (z, x, y, segments, j, size, key) => {
-  const geometry = new PlaneBufferGeometry(size, size, segments + 2, segments + 2)
-  // const geometry = new PlaneBufferGeometry( size, size, 4, 4);
-
-  heightmap(z, x, y).then(heightmap => {
-    setHeightmap(geometry, heightmap, 0.1, 0, key)
+  fetchPNG(z, x, y).then(png => {
+    buildTile(png, size, segments, key)
   })
 }
